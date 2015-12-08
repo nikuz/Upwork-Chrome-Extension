2b777dbb11b61ae12e1c53c1c0f9ae59aaa8d78a
nikuz <micurino@gmail.com>
2015-12-08 10:30:13 +0530

    gulpfile optimize

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,12 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
   fs = require('fs'),
-  livereload = require('gulp-livereload'),
   webpack = require('webpack'),
-  WebpackDevServer = require('webpack-dev-server'),
   webpackConfig = require('./webpack.config.js'),
-  eslint = require('gulp-eslint');
+  eslint = require('gulp-eslint'),
+  clean = require('gulp-clean');
 
-gulp.task('default', ['webpack-dev-server']);
-
-gulp.task('webpack:build', function(callback) {
+gulp.task('webpack:build', ['build:clean'], function(done) {
   // modify some webpack config options
   var myConfig = Object.create(webpackConfig);
   myConfig.plugins = myConfig.plugins.concat(
@@ -32,11 +29,15 @@ gulp.task('webpack:build', function(callback) {
     gutil.log('[webpack:build]', stats.toString({
       colors: true
     }));
-    callback();
+    done();
   });
 });
 
-gulp.task('copy:manifest', function(done) {
+gulp.task('build:clean', function() {
+  return gulp.src('./release', {read: false}).pipe(clean({force: true}));
+});
+
+gulp.task('copy:manifest', ['build:clean'], function(done) {
   var manifestFile = 'manifest.json',
     releaseFolder = 'release',
     manifest = require(`./${manifestFile}`),
@@ -68,27 +69,23 @@ gulp.task('copy:manifest', function(done) {
   fs.writeFileSync(`./${releaseFolder}/${manifestFile}`, JSON.stringify(manifest, null, 2));
   done();
 });
-gulp.task('copy:manifest-dev', function() {
-  gulp.src(['./manifest.json']).pipe(gulp.dest('release'));
+
+gulp.task('copy:manifest-dev', ['build:clean'], function() {
+  return gulp.src(['./manifest.json']).pipe(gulp.dest('release'));
 });
 
-gulp.task('copy:images', function() {
-  gulp.src(['./src/images/**/*']).pipe(gulp.dest('release/images'));
+gulp.task('copy:images', ['build:clean'], function() {
+  return gulp.src(['./src/images/**/*']).pipe(gulp.dest('release/images'));
 });
 
-gulp.task('build', ['webpack:build', 'copy:manifest', 'copy:images']);
-
-// modify some webpack config options
 var myDevConfig = Object.create(webpackConfig);
 myDevConfig.devtool = 'sourcemap';
 myDevConfig.debug = true;
 
-// create a single instance of the compiler to allow caching
 var devCompiler = webpack(myDevConfig);
 
-gulp.task('webpack:build-dev', function(callback) {
-  // run webpack
-  devCompiler.run(function(err, stats) {
+gulp.task('webpack:build-dev', ['build:clean'], function(callback) {
+  return devCompiler.run(function(err, stats) {
     if(err) throw new gutil.PluginError('webpack:build-dev', err);
     gutil.log('[webpack:build-dev]', stats.toString({
       colors: true
@@ -97,50 +94,22 @@ gulp.task('webpack:build-dev', function(callback) {
   });
 });
 
-// Build and watch cycle (another option for development)
-// Advantage: No server required, can run app from filesystem
-// Disadvantage: Requests are not blocked until bundle is available,
-//               can serve an old app on refresh
-gulp.task('build-dev', ['copy:manifest-dev', 'copy:images', 'webpack:build-dev'], function() {
-  gulp.watch(['src/**/*'], ['webpack:build-dev']);
-});
-
-gulp.task('webpack-dev-server', function(callback) {
-  // modify some webpack config options
-  var myConfig = Object.create(webpackConfig);
-  myConfig.devtool = 'cheap-module-eval-source-map';
-  myConfig.debug = true;
-  myConfig.cache = true;
-  myConfig.entry.main.unshift('webpack-dev-server/client?http://localhost:8080/', 'webpack/hot/only-dev-server');
-  myConfig.plugins.unshift(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  );
-
-  // Start a webpack-dev-server
-  new WebpackDevServer(webpack(myConfig), {
-    //quiet: true,
-    stats: {
-      colors: true
-    }
-  }).listen(8080, 'localhost', function(err) {
-    if (err) throw new gutil.PluginError('webpack-dev-server', err);
-  });
+gulp.task('build-dev', ['build:clean'], function() {
+  return gulp.watch(['src/**/*'], ['webpack:build-dev']);
 });
 
 gulp.task('eslint', function () {
   return gulp.src([
-    'app/**/*.js',
-    'src/**/*.js',
-    'src/**/*.jsx'
-  ])
-    // eslint() attaches the lint output to the eslint property
-    // of the file object so it can be used by other modules.
+      'app/**/*.js',
+      'src/**/*.js',
+      'src/**/*.jsx'
+    ])
     .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
 });
+
+
+gulp.task('build', ['build:clean', 'webpack:build', 'copy:manifest', 'copy:images']);
+
+gulp.task('default', ['build:clean', 'copy:manifest-dev', 'copy:images', 'webpack:build-dev', 'build-dev']);
